1e08636bd795be66dd0bd313263fc75c7b3020f0
tungluu18 <tungluu18@gmail.com>
2018-08-25 21:47:54 +0700

    function vote for candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -12,6 +12,10 @@ contract Election {
 
   // Fetch Candidate  
   mapping(uint => Candidate) public candidates;
+  
+  // Store accounts that have voted
+  mapping(address => bool) public voters;
+
   // Store Candidate Count
   uint public candidatesCount;
   // Read Candidate
@@ -25,5 +29,19 @@ contract Election {
   function addCandidate (string _name) private {
     candidatesCount++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote (uint _candidateId) public {
+    // check whether account has voted
+    require(!voters[msg.sender] == true);
+
+    // candidate must be valid
+    require(0 < _candidateId && _candidateId <= candidatesCount);
+
+    // record that account has voted
+    voters[msg.sender] = true;
+
+    //vote for the candidate
+    candidates[_candidateId].voteCount++;
   }
 }
